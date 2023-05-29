71de6974b347275b36b3640b023bbe317de69cea
Rajnish Ranjan <rajnishr@interrait.com>
2023-05-29 14:24:32 +0530

    Update

--- src/Overriding/RBI.java
+++ src/Overriding/RBI.java
@@ -1,18 +1,5 @@
 package Overriding;
 
 public class RBI {
-	
-public double getHomeLoanROI() {
-
-return 8.5;
-}
-
-public double getCarLoanROI() {
-	return 10.5;
-}
-public RBI getObject() {
-	RBI obj = new RBI();
-	return obj;
-	
-}
+	//jimijjimi
 }
