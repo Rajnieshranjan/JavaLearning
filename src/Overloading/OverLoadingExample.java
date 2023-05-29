95c775898185b57917bfc7b96c4785542ed3d8ef
Rajnish Ranjan <rajnishr@interrait.com>
2023-05-29 13:37:08 +0530

    Update JavaPractice

--- src/Overloading/OverLoadingExample.java
+++ src/Overloading/OverLoadingExample.java
@@ -8,24 +8,49 @@ public class OverLoadingExample {
 		return a+b;
 	}
 	public int add (int a, int b, int c) {
+		
 		return a+b+c;
 		
 	}
 	public String doLogin(String userName,String password) {
+		
 		return userName+password;
 		
 	}
 
 	public int doLogin(int mobileNumber, int password) {
+		System.out.println("ahsdsahd");
 		return mobileNumber+password;
+	}
+
+	
+	public static void main(int a, int b){
+		System.out.println("intab");
+		
+		OverLoadingExample obj= new OverLoadingExample();
+		System.out.println(obj.doLogin(123, 0));
+		System.out.println(obj.add(3, 4));
+		System.out.println(obj.add(4, 5, 6));
+		System.out.println(obj.doLogin(455666, 7899000));
 	}
 
 	public static void main (String [] args) {
 
 		OverLoadingExample obj= new OverLoadingExample();
 		System.out.println(obj.doLogin(123, 0));
-		System.out.println(obj.add(3, 4));
-		System.out.println(obj.add(4, 5, 6));
+	System.out.println(obj.add(3, 4));
+	System.out.println(obj.add(4, 5, 6));
 		System.out.println(obj.doLogin(455666, 7899000));
+		
+		main(1,2,3);
+		System.out.println(obj.doLogin(1, 1));
+		
+		
+		System.out.println("ckjefhgyuadfbhadfygu");
+	}
+	public static void main (int a, int b, int c) {
+		int d=a*b*c;
+		System.out.println("intabcdef");
+		
 	}
 }   
--- src/Overriding/HDFC.java
+++ src/Overriding/HDFC.java
@@ -4,15 +4,15 @@ public class HDFC extends RBI {
 	
 	public static void main (String[] args) {
 		
-		HDFC obj = new HDFC();
+		RBI obj = new RBI();
 		System.out.println(obj.getHomeLoanROI());
 		System.out.println(obj.getCarLoanROI());
 	}
 	public double getHomeLoanROI() {
 
 		return 11;
+	}
 
-}
 public HDFC getObject(){
 	HDFC obj= new HDFC();
 	return obj;
